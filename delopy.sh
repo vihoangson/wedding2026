0aa01abae9c50a26042a4cf3bd6a6fc9bc1240d5
AzureAD\HoangSon <hoang_son@lampart-vn.com>
2026-06-30 14:17:26 +0700

    commit_message

--- delopy.sh
+++ delopy.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "commit_message"
 git push
-ssh root@188.166.213.71 -i ../002.pem "cd /var/www/vhosts/hoangson-yennhi.oop.vn && git pull && service httpd restart"
+ssh root@188.166.213.71 -i ../002.pem "cd /var/www/vhosts/hoangson-yennhi.oop.vn && git fetch && git checkout wedding_main&& git pull && service httpd restart"
