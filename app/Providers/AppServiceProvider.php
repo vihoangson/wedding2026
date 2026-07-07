5b8560aba3ed54455efc46429c9e88be2c1008ac
AzureAD\HoangSon <hoang_son@lampart-vn.com>
2026-07-07 10:51:29 +0700

    commit_message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,37 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->initializeDataFiles();
+    }
+
+    /**
+     * Initialize missing data files from .example templates.
+     * If a .json file doesn't exist but .json.example does, copy it.
+     *
+     * @return void
+     */
+    private function initializeDataFiles()
+    {
+        $dataDir = storage_path('../data');
+
+        if (!is_dir($dataDir)) {
+            return;
+        }
+
+        $requiredFiles = [
+            'data.json',
+            'comments.json',
+            'memories.json',
+            'memory_comments.json',
+        ];
+
+        foreach ($requiredFiles as $file) {
+            $filePath = $dataDir . DIRECTORY_SEPARATOR . $file;
+            $examplePath = $dataDir . DIRECTORY_SEPARATOR . $file . '.example';
+
+            if (!file_exists($filePath) && file_exists($examplePath)) {
+                copy($examplePath, $filePath);
+            }
+        }
     }
 }
