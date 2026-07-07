51e3d5cf03844a24482bbef1c00ab386cbb27fc1
AzureAD\HoangSon <hoang_son@lampart-vn.com>
2026-07-07 10:44:58 +0700

    commit_message

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+git add .
+git commit -m "commit_message"
+git push
+ssh root@188.166.213.71 -i ../002.pem "cd /var/www/vhosts/hoangson-yennhi.oop.vn && git fetch && git checkout laravel_template && git pull && service httpd restart"
